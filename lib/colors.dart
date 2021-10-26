55680a7183ff73010522b46bd77da939fb81acda
atharsajid <athersajid620@gmail.com>
2021-10-26 09:04:37 -0700

    update

---
+++ lib/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color backgroundcolor = Colors.grey.shade100;
---
+++ lib/homescreen.dart/homescreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/colors.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundcolor,
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
